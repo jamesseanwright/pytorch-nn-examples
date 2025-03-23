e51f14e4bbb4233ad2d31670cdb5f922ca55e475
James Wright <jsw@mahoganyteeth.com>
2025-03-23 21:24:33 +0000

    Converts one-hot zeroed array dtype to float32

--- charlm/onehot.py
+++ charlm/onehot.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.typing import NDArray
 
 def one_hot_encoder(encoded_text: NDArray[any], n_unique_char: int):
-    one_hot = np.zeros((encoded_text.size, n_unique_char))
+    one_hot = np.zeros((encoded_text.size, n_unique_char)).astype(np.float32)
     one_hot[np.arange(one_hot.shape[0]), encoded_text.flatten()] = 1.0
     one_hot = one_hot.reshape((*encoded_text.shape, n_unique_char))
 
